766460ad1d9b86ead10512a640bc0ba9cc5c340c
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-27 00:17:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import logging
 import requests
 import schedule
 import time
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
+
+# 设置日志级别和格式
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_access_token(host, email, password):
     url = f'http://{host}/auth/token'
@@ -17,7 +22,7 @@ def get_access_token(host, email, password):
         access_token = response.json().get('access_token')
         return access_token
     else:
-        print('Failed to get access token')
+        logging.error('Failed to get access token')
         return None
 
 def push_token_to_cloudflare_worker(access_token):
@@ -37,21 +42,21 @@ def push_token_to_cloudflare_worker(access_token):
             script_variable_name: access_token
         }
     }
-        
+
     url = f'https://api.cloudflare.com/client/v4/accounts/{account_id}/workers/scripts/{script_name}/variable'
     response = requests.put(url, headers=headers, json=payload)
-        
+
     if response.status_code == 200:
-        print('Token pushed to Cloudflare worker successfully')
+        logging.info('Token pushed to Cloudflare worker successfully')
     else:
-        print('Failed to push token to Cloudflare worker')
+        logging.error('Failed to push token to Cloudflare worker')
 
 def refresh_token_and_push(host, email, password):
     access_token = get_access_token(host, email, password)
     if not access_token:
-        print('Failed to get access token')
+        logging.error('Failed to get access token')
         return
-    
+
     url = f'http://{host}/auth/refresh_token'
     headers = {
         'Authorization': 'Bearer ' + access_token
@@ -61,7 +66,7 @@ def refresh_token_and_push(host, email, password):
         access_token = response.json().get('access_token')
         push_token_to_cloudflare_worker(access_token)
     else:
-        print('Failed to refresh access token')
+        logging.error('Failed to refresh access token')
 
 def main():
     host = os.getenv('HOST')
@@ -69,11 +74,11 @@ def main():
     password = os.getenv('PASSWORD')
 
     if not host or not email or not password:
-        print('Please provide HOST, EMAIL, and PASSWORD in the .env file')
+        logging.error('Please provide HOST, EMAIL, and PASSWORD in the .env file')
         sys.exit(1)
-    
+
     schedule.every().week.do(refresh_token_and_push, host=host, email=email, password=password)
-    
+
     while True:
         schedule.run_pending()
         time.sleep(1)
