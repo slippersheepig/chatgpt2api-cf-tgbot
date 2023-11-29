7e56ee3d4c8ad52eeb4a1da5a16ae7e18cb56466
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-29 19:58:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,72 @@
+import os
 import logging
-import requests
 import schedule
 import time
-import os
+import requests
 from dotenv import load_dotenv
-import sys
-import urllib.parse
 
 load_dotenv()
 
 # 设置日志级别和格式
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_access_token(host, email, password):
-    url = f'http://{host}/auth/token'
+BASE_URL = os.getenv('BASE_URL')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+CF_API_TOKEN = os.getenv('CF_API_TOKEN')
+CF_ACCOUNT_ID = os.getenv('CF_ACCOUNT_ID')
+CF_SCRIPT_NAME = os.getenv('CF_SCRIPT_NAME')
+CF_SCRIPT_VARIABLE_NAME = os.getenv('CF_SCRIPT_VARIABLE_NAME')
+
+def login():
+    url = f'{BASE_URL}/chatgpt/login'
     data = {
-        'username': email,
-        'password': password,
+        'username': USERNAME,
+        'password': PASSWORD
     }
-    response = requests.post(url, data=urllib.parse.urlencode(data), headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    response = requests.post(url, json=data)
     if response.status_code == 200:
-        access_token = response.json().get('access_token')
+        access_token = response.json().get('accessToken')
         return access_token
     else:
-        logging.error('Failed to get access token')
+        logging.error('登录失败')
         return None
 
 def push_token_to_cloudflare_worker(access_token):
-    cf_api_token = os.getenv('CF_API_TOKEN')
-    account_id = os.getenv('CF_ACCOUNT_ID')
-    script_name = os.getenv('CF_SCRIPT_NAME')
-    script_variable_name = os.getenv('CF_SCRIPT_VARIABLE_NAME')
-
     headers = {
-        'Authorization': f'Bearer {cf_api_token}',
+        'Authorization': f'Bearer {CF_API_TOKEN}',
         'Content-Type': 'application/json'
     }
 
     # 更新 Cloudflare worker 中的变量
     payload = {
         'vars': {
-            script_variable_name: access_token
+            CF_SCRIPT_VARIABLE_NAME: access_token
         }
     }
 
-    url = f'https://api.cloudflare.com/client/v4/accounts/{account_id}/workers/scripts/{script_name}/variable'
+    url = f'https://api.cloudflare.com/client/v4/accounts/{CF_ACCOUNT_ID}/workers/scripts/{CF_SCRIPT_NAME}/variable'
     response = requests.put(url, headers=headers, json=payload)
 
     if response.status_code == 200:
-        logging.info('Token pushed to Cloudflare worker successfully')
+        logging.info('访问令牌成功刷新并推送到 Cloudflare worker')
     else:
-        logging.error('Failed to push token to Cloudflare worker')
+        logging.error('访问令牌刷新并推送到 Cloudflare worker 失败')
 
-def refresh_token_and_push(host, email, password):
-    access_token = get_access_token(host, email, password)
+def refresh_token_and_push():
+    access_token = login()
     if not access_token:
-        logging.error('Failed to get access token')
+        logging.error('登录失败')
         return
 
-    url = f'http://{host}/auth/refresh_token'
-    headers = {
-        'Authorization': 'Bearer ' + access_token
-    }
-    response = requests.post(url, headers=headers)
-    if response.status_code == 200:
-        access_token = response.json().get('access_token')
-        push_token_to_cloudflare_worker(access_token)
-    else:
-        logging.error('Failed to refresh access token')
+    push_token_to_cloudflare_worker(access_token)
 
 def main():
-    host = os.getenv('HOST')
-    email = os.getenv('EMAIL')
-    password = os.getenv('PASSWORD')
-
-    if not host or not email or not password:
-        logging.error('Please provide HOST, EMAIL, and PASSWORD in the .env file')
-        sys.exit(1)
-
-    # 获取并推送 access_token
-    access_token = get_access_token(host, email, password)
+    access_token = login()  # 首次运行立即更新变量
     if access_token:
         push_token_to_cloudflare_worker(access_token)
-    else:
-        logging.error('Failed to get access token')
 
-    # 每周定时刷新并推送 access_token
-    schedule.every().week.do(refresh_token_and_push, host=host, email=email, password=password)
+    schedule.every().week.do(refresh_token_and_push)
 
     while True:
         schedule.run_pending()
